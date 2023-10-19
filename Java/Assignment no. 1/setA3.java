959d1cab76a1cf27494a06a2f921ddaf89f4f64c
Tejal2025 <147473934+Tejal2025@users.noreply.github.com>
2023-10-19 13:53:53 +0530

    Update setA3.java

--- Java/Assignment no. 1/setA3.java
+++ Java/Assignment no. 1/setA3.java
@@ -1,63 +1,116 @@
 import java.util.*;
-import java.util.*;
-
-class switch_case
+class setA3
 {
-    public static void main(String[]args)
-    {
-      int length,breadth;
-      Scanner sc=new Scanner(System.in);
-      
-      int r,h,fact=1,op=1,num;
-      float PI=3.14f;
-      while(op >=1 && op <= 4)
-      {
-        System.out.println("1:Volume of cylinder :");
-        System.out.println("2:Factorial of number:");
-        System.out.println("3:Armstrong or not :");
-        System.out.println("4:Exit");
-        System.out.println("\n Enter option:");
-        op=sc.nextInt();
-        
-        switch(op)
-        {
-           case 1:
-                 System.out.println("Enter radius of cylinder:");
-                 r=sc.nextInt();
-                 System.out.println("Enter height of cylinder:");
-                 h=sc.nextInt();
-                 System.out.println("Volume of cylinder="+PI*r*r*h);
-                 break;
-                 
-           case 2:
-                 System.out.println("Enter number to find factorial:");
-                 num=sc.nextInt();
-                 for(int i=0;i<num;i++)
-                     fact=fact*i;
-                     System.out.println("\n Factorial of number:"+fact);
-                break;
-      
-           case 3:
-                  System.out.println("Enter the number to find the factorial:");
-                  num=sc.nextInt();
-                  int rem,num1,res=0;
-                  num1=num;
-                  while(num1 !=0)
-                  {
-                     rem=num1 %10;
-                     res=res+(rem*rem*rem);
-                     num1/=10;
-                  }
-                  if(res==num)
-                     System.out.println("\n "+num+"%d is armstrong number:");
-                  else
-                     System.out.println("\n "+num+"%d is not armstrong number:");
-                  break;
-            
-           case 4:
-                  System.exit(0);
-        }   
-     }
-  }
+	static void cylinder()
+	{
+	    float r,h;
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter radius of cylinder :");
+	    r=sc.nextFloat();
+	    System.out.println("Enter height of cylinder :");
+	    h=sc.nextFloat();
+	    System.out.println("Volume of cylinder =:"+3.142*r*r*h);
+	}
+	static void fact()
+	{
+	    int n,fact=1;
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter the number :");
+	    n=sc.nextInt();
+	    for(int i=1;i<=n;i++)
+	    {
+	       fact=fact*i;
+	    }
+	    System.out.println("Factorial of "+n+" is :"+fact);
+	}
+	static void armstrong()
+	{
+	    int n,sum=0,num;
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter the number :");
+	    n=sc.nextInt();
+	    int no=n;
+	    while(n!=0)
+	    {
+	       num=n%10;
+	       sum=sum+(num*num*num);
+	       n=n/10;
+	    }
+	    if(sum==no)
+	    {
+	       System.out.println("Number"+ no +"is armstrong number");
+	    }
+	    else 
+	       System.out.println("Number"+ no +"is not armstrong number");
+	    }
+	    
+	public static void main(String a[])
+	{
+	    int ch=0;
+	    Scanner sc=new Scanner(System.in);
+	    while(ch<=3)
+	    {
+	    System.out.println("1.Volume of cylinder:");
+	    System.out.println("2.Factorial of Number:");
+	    System.out.println("3.Number is Armstrong or not:");
+	    System.out.println("4.Exit:");
+	    
+	 System.out.println("Enter Your Choice:");
+	 ch=sc.nextInt();
+	 
+	 switch(ch)
+	 {
+	    case 1:cylinder();
+	           break;
+	    case 2:fact();
+	           break;
+	    case 3:armstrong();
+	           break;
+	 }
+       }
+    }
 }
-      
+
+/*
+ty32@pc38:~/TYBCS-32/JAVA/Assignment No 1$ javac setA3.java
+ty32@pc38:~/TYBCS-32/JAVA/Assignment No 1$ java setA3	       
+1.Volume of cylinder:
+2.Factorial of Number:
+3.Number is Armstrong or not:
+4.Exit:
+Enter Your Choice:  1
+Enter radius of cylinder : 7
+Enter height of cylinder : 4
+Volume of cylinder =:615.832
+
+1.Volume of cylinder:
+2.Factorial of Number:
+3.Number is Armstrong or not:
+4.Exit:
+Enter Your Choice: 2
+Enter the number :4
+Factorial of 4 is :24
+
+1.Volume of cylinder:
+2.Factorial of Number:
+3.Number is Armstrong or not:
+4.Exit:
+Enter Your Choice: 3
+Enter the number :150
+Number 150 is not armstrong number
+
+1.Volume of cylinder:
+2.Factorial of Number:
+3.Number is Armstrong or not:
+4.Exit:
+Enter Your Choice: 3
+Enter the number : 153
+Number 153 is armstrong number
+
+1.Volume of cylinder:
+2.Factorial of Number:
+3.Number is Armstrong or not:
+4.Exit:
+Enter Your Choice: 4
+*/	    
+	        
